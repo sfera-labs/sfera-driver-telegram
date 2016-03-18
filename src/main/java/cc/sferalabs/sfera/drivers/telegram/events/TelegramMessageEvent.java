34cdcd1d3d456b5a7c48ac6255fd5618266eff76
Giampiero <giampiero7@users.noreply.github.com>
2016-03-18 17:27:21 +0100

    POMs fix

--- src/main/java/cc/sferalabs/sfera/drivers/telegram/events/TelegramMessageEvent.java
+++ src/main/java/cc/sferalabs/sfera/drivers/telegram/events/TelegramMessageEvent.java
@@ -10,6 +10,8 @@ import cc.sferalabs.sfera.events.BaseEvent;
 /**
  * Event triggered when the Telegram Bot receives a message.
  * 
+ * @sfera.event_id message
+ * 
  * @author Giampiero Baggiani
  *
  * @version 1.0.0
@@ -31,11 +33,24 @@ public class TelegramMessageEvent extends BaseEvent implements TelegramEvent {
 		this.message = message;
 	}
 
+	/**
+	 * Returns the {@link Message} object representing the message that
+	 * triggered this event
+	 * 
+	 * @return the {@link Message} object representing the message that
+	 *         triggered this event
+	 */
 	@Override
 	public Message getValue() {
 		return message;
 	}
 
+	/**
+	 * Returns the text of the message that triggered this event, if any.
+	 * 
+	 * @return the text of the message that triggered this event, or
+	 *         {@code null} if the message did not contain any text
+	 */
 	@Override
 	public String getScriptConditionValue() {
 		return message.getText();
